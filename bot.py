f5b4d40df37f6023b88d673dad0e1eecb4465ee3
Jan Doležal <jandolezal@email.cz>
2021-01-23 17:36:21 +0100

    Remove xmltodict parts

--- bot.py
+++ bot.py
@@ -6,16 +6,17 @@ import tweepy
 
 load_dotenv()
 
-# Get energy from past hour. Script will be a cron job run at HH:05
+# Get energy from past hour. Script will be a cronjob 10 7-18 * * *
 start = (datetime.datetime.utcnow() - datetime.timedelta(hours=1)).isoformat(timespec='hours')
 end = datetime.datetime.utcnow().isoformat(timespec='hours')
+# %2F as backslash for the get request timeinterval parameter
 past_hour = start + '%2F' + end
 
 # Update params to make a Entsoe API request
 params['securityToken'] = os.getenv('ENTSOE_TOKEN')
 params['TimeInterval'] = past_hour
 
-#Get energy for past hour from Entsoe API
+# Get energy for past hour from Entsoe API
 data = request_data(url, params)
 energy = parse_xml(data, res_map)
 
@@ -25,11 +26,11 @@ if data_check(energy):
     consumer_secret = os.getenv('CONSUMER_SECRET')
     access_token = os.getenv('ACCESS_TOKEN')
     access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
-    
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    
+
     # Tweet the data
     tweet = f"🌬️ {energy['Wind Onshore']} MWh\n" + \
             f"☀️ {energy['Solar']} MWh\n" + \
--- entsoe.py
+++ entsoe.py
@@ -1,12 +1,13 @@
 import requests
 import xml.etree.ElementTree as ET
-import xmltodict
+
 
 def request_data(url, params):
     r = requests.get(url, params=params)
     if r.status_code != 200:
         return None
     return r.text
+
 
 def parse_xml(xml, res_map):
     """Parses renewable energy from xml string.
@@ -15,23 +16,12 @@ def parse_xml(xml, res_map):
     root = ET.fromstring(xml)
     ns = '{urn:iec62325.351:tc57wg16:451-6:generationloaddocument:3:0}'
     for serie in root.iter(ns + 'TimeSeries'):
-        psr_type = serie.find(ns + 'MktPSRType').find(ns+ 'psrType').text
+        psr_type = serie.find(ns + 'MktPSRType').find(ns + 'psrType').text
         quantity = serie.find(ns + 'Period').find(ns + 'Point').find(ns + 'quantity').text
-        if psr_type in res_map:
-                energy[res_map[psr_type]] = quantity
-    return energy
-
-def parse_xml_as_dict(xml, res_map):
-    """Parses renewable energy from xml string.
-    """
-    energy = {}
-    d = xmltodict.parse(xml)
-    for serie in d['GL_MarketDocument']['TimeSeries']:
-        psr_type = serie['MktPSRType']['psrType']
-        quantity = serie['Period']['Point']['quantity']
         if psr_type in res_map:
             energy[res_map[psr_type]] = quantity
     return energy
+
 
 def data_check(energy):
     for entry in energy.values():
@@ -41,7 +31,9 @@ def data_check(energy):
             return False
     return True
 
+
 url = 'https://transparency.entsoe.eu/api?'
+
 
 params = {
     'securityToken': None,
@@ -50,6 +42,7 @@ params = {
     'DocumentType': 'A75',
     'TimeInterval': None,
     }
+
 
 res_map = {
     'B01': 'Biomass',
